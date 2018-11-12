4fc11548d1d0432c5313d4c6a56690f7f1548686
Max Isom <code@maxisom.me>
2018-11-12 15:07:42 -0500

    Inital commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Website for Open Source Hardware
+
+### Development
+
+This project uses [Gulp](http://gulpjs.com) to run automated tasks for development and production builds.
+The tasks are as follows:
+
+`gulp --production`: Same as `gulp serve --production` also run `gulp test` and  not boot up production server
+
+`gulp serve`: Compiles preprocessors and boots up development server
+`gulp serve --production`: Same as `gulp serve` but will run all production tasks so you can view the site/app in it's final optimized form
+
+## Text
+
+A student organization dedicated to the development and availability of open source hardware.
+
+- Who are we?
+  - We are a Michigan Technological University enterprise specializing in the development and availability of open source hardware.
+- What do we do?
+  - We are a product development firm that offers design and prototyping through the use of open source software and hardware like 3D printers. Being open source means the instructions we use, the 3D models we create, and the software we write are shared to the commons, where they can be modified or redistributed. We charge a reasonably high design rate up front so that we can make a profit, and then everyone after us reaps the benefits of our work for a massive discount if they choose to recreate it. Anyone who so desires can make changes or updates to our parts, and through this process our parts are improved at a much higher rate than would be possible by ourselves. Open source is all about collaboration.
+
+
+
+  Current Projects
+
+We currently have many cool projects we are pursuing.
+
+    GM Automation Cage (Automated 3D printer with a robotic arm)
+    Recyclebot V5
+    Granulator
+    Lasersaur
+    Low cost insulation
+    Solar
+    High-temperature printing
+
+
+
+    Future
+
+    We believe OSHE has a very bright future. We’ve decided to start building our foundations strong so that we can tackle a wide range of problems, and offer an even wider range of solutions. Current future projects include:
+
+        Building more 3D printers
+        Becoming more involved in Open Source Ecology
+        Hosting workshops on Open Source Ideology
+        Designing/Building a wide range of solutions for customers
+        Building demonstration products for the community
+            Progressive Vision Loss Lenses
+            Robotic Arms Within 3D Printing Process
+            Recylebot v5 Filament Extruder
+            Movable 3D printers
+            Granulator v2
+
+
+            Rates
+            Group Type 	Filament 	Design and Development
+            Internal (University) 	$0.05 per gram 	$10/hr
+            External (Non-university related) 	$0.10 per gram 	$25/hr
+
+
+  Order form
+
+  team roster
---
+++ gulpfile.js
@@ -0,0 +1,122 @@
+const gulp = require('gulp');
+const del = require('del');
+
+// Flags
+const minimist = require('minimist');
+const knownOptions = {
+  string: 'env',
+  default: { env: process.env.NODE_ENV || 'development' }
+};
+const options = minimist(process.argv.slice(2), knownOptions);
+
+// Plugins
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
+const cssnano = require('gulp-cssnano');
+const newer = require('gulp-newer');
+const imagemin = require('gulp-imagemin');
+const nunjucksRender = require('gulp-nunjucks-render');
+const htmlmin = require('gulp-htmlmin');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const runSequence = require('run-sequence');
+
+// Sass
+gulp.task('sass', () => {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass({ includePaths: ['node_modules'] }).on('error', sass.logError))
+    .pipe(concat('combined.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
+
+// JS
+gulp.task('js', () => {
+  return gulp.src('app/js/**/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(concat('combined.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+// Images
+gulp.task('images', () => {
+  return gulp.src('app/images/**/*')
+    .pipe(newer('dist/images/'))
+    .pipe(imagemin({ optimizationLevel: 5 }))
+    .pipe(gulp.dest('dist/images/'));
+});
+
+// Nunjucks
+gulp.task('nunjucks', () => {
+  return gulp.src('app/pages/**/*.+(html|nunjucks)')
+    // Renders template with nunjucks
+    .pipe(nunjucksRender({
+      path: ['app/pages/templates']
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+// Minify
+gulp.task('minify', () => {
+  return gulp.src('dist/**/*')
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulpIf('*.html', htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    })))
+    .pipe(gulp.dest('dist'));
+});
+
+// Clean dist folder
+gulp.task('build:clean', () => {
+  return del.sync('dist/*');
+})
+
+// Basic dev build
+gulp.task('build', (callback) => {
+  runSequence(['sass', 'js', 'images', 'nunjucks'], callback);
+});
+
+// Build and reload
+gulp.task('build:live', (callback) => {
+  if (options.env === 'production') {
+    runSequence('build', 'minify', () => {
+      browserSync.reload();
+      callback();
+    });
+  } else {
+    runSequence('build', () => {
+      browserSync.reload();
+      callback();
+    });
+  }
+});
+
+// Production build with minification
+gulp.task('build:production', (callback) => {
+  runSequence('build:clean', 'build', 'minify', callback);
+});
+
+// Watch
+gulp.task('serve', () => {
+  runSequence('build:clean', 'build:live', () => {
+    // Init Browsersync
+    browserSync.init({
+      server: 'dist'
+    });
+
+    // Sass
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+
+    // Nunjucks
+    gulp.watch('app/pages/**/*.nunjucks', ['build:live']);
+
+    // JS
+    gulp.watch('app/js/**/*.js', ['build:live']);
+  });
+});
